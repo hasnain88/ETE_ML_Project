049e59967aadd5749d8069c2ac4fa3ce76ae7d32
Hasnain Narsandawala <hasnain.narsandawala@gmail.com>
2024-03-21 09:02:13 +0530

    Data Transformation

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -7,6 +7,8 @@ from dotenv import load_dotenv
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import r2_score
 import pymysql
+import pickle
+import numpy as np
 
 
 
@@ -39,3 +41,15 @@ def read_sql_data():
 
     except Exception as ex:
         raise CustomException(ex)
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
